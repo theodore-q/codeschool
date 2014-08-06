b003dd85f1f39956f79ce917bf6539d1e3c6d195
theodore-q <theo.q.er@gmail.com>
2014-08-06 13:32:34 +0100

    ship

--- exersise2/app.js
+++ exersise2/app.js
@@ -1,31 +1,31 @@
-   //  (function () {
-   //      var app = angular.module('TeaStore', []);
+    (function () {
+        var app = angular.module('TeaStore', []);
 
-   //      app.controller('StoreController', function(){
-			// this.product = Tea;
-   //      });
+        app.controller('StoreController', function(){
+			this.product = Tea;
+        });
 
-   //      var Tea = {
-			// name: 'Oolong',
-			// price: 2.60,
-			// description: 'Oolong is neither green nor black',
-   //      };
-
-   //  })();
-
-    (function(){
-      var app = angular.module('gemStore', []);
-
-      app.controller('StoreController', function () {
-        this.product = gem;
-      });
-
-      var gem = {
-        name: "Dodecahedron",
-        price: 2.95,
-        description: '. . .',
-      }
-
-console.log(app);
+        var Tea = {
+			name: 'Oolong',
+			price: 2.60,
+			description: 'Oolong is neither green nor black',
+        };
 
     })();
+
+//     (function(){
+//       var app = angular.module('gemStore', []);
+
+//       app.controller('StoreController', function () {
+//         this.product = gem;
+//       });
+
+//       var gem = {
+//         name: "Dodecahedron",
+//         price: 2.95,
+//         description: '. . .',
+//       }
+
+// console.log(app);
+
+//     })();
