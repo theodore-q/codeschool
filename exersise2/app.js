247eb95e3b07ecd2310510d4de72ee3106338a09
theodore-q <theo.q.er@gmail.com>
2014-08-06 14:21:25 +0100

    hunger

--- exersise2/app.js
+++ exersise2/app.js
@@ -11,6 +11,7 @@
         price: 2.95,
         description: 'Oolong is neither green nor black',
         canPurchase: true,
+        soldOut: false,
         
       },
       {
@@ -18,12 +19,14 @@
         price: 2.95,
         description: 'The champagne of tea',
         canPurchase: true,
+        soldOut: false,
       },
       {
         name: "Sencha",
         price: 2.95,
         description: 'A crisp green tea',
         canPurchase: true,
+        soldOut: false,
       },
 ];
 
