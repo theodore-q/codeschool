915008e1b6f3562a6c36725ce3d1f5784ae29344
theodore-q <theo.q.er@gmail.com>
2014-08-06 13:06:15 +0100

    yo

--- exersise2/app.js
+++ exersise2/app.js
@@ -1,14 +1,28 @@
-    (function () {
-        var app = angular.module('TeaStore', []);
+   //  (function () {
+   //      var app = angular.module('TeaStore', []);
 
-        app.controller('StoreController', function(){
-			this.product = Tea;
-        });
-        
-        var Tea = {
-			name: 'Oolong',
-			price: 2.60,
-			description: 'Oolong is neither green nor black',
-        };
+   //      app.controller('StoreController', function(){
+			// this.product = Tea;
+   //      });
 
+   //      var Tea = {
+			// name: 'Oolong',
+			// price: 2.60,
+			// description: 'Oolong is neither green nor black',
+   //      };
+
+   //  })();
+
+        (function(){
+      var app = angular.module('gemStore', []);
+
+      var gem = {
+        name: "Dodecahedron",
+        price: 2.95,
+        description: '. . .',
+      }
+
+      app.controller('StoreController', function () {
+        this.product = gem;
+      });
     })();
