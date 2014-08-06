edcb0e877c4114695847fff5601f91e2a093dbb0
theodore-q <theo.q.er@gmail.com>
2014-08-06 14:18:56 +0100

    hunger

--- exersise2/app.js
+++ exersise2/app.js
@@ -11,21 +11,19 @@
         price: 2.95,
         description: 'Oolong is neither green nor black',
         canPurchase: true,
-        soldOut: false,
+        
       },
       {
         name: "Darjeeling",
         price: 2.95,
         description: 'The champagne of tea',
         canPurchase: true,
-        soldOut: false,
       },
       {
         name: "Sencha",
         price: 2.95,
         description: 'A crisp green tea',
         canPurchase: true,
-        soldOut: false,
       },
 ];
 
