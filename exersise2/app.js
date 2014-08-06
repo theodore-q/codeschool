cfc11dbeeecefd535f4136fa02385b419f871785
theodore-q <theo.q.er@gmail.com>
2014-08-06 14:33:01 +0100

    hunger

--- exersise2/app.js
+++ exersise2/app.js
@@ -2,7 +2,7 @@
       var app = angular.module('gemStore', []);
 
       app.controller('StoreController', function () {
-        this.product = gems;
+        this.products = gems;
       });
 
       var gems = [
