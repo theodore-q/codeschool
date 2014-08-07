30646fd959ca6db7e105ccc252470d530c25014a
theodore-q <theo.q.er@gmail.com>
2014-08-07 23:57:23 +0100

    late into the night

--- exersise2copy/app.js
+++ exersise2copy/app.js
@@ -6,10 +6,12 @@
            $http.JSONP('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&q=http://www.telegraph.co.uk/news/uknews/rss').
         }).success(function(data, status, headers, config) {
           // data contains the response
-          $scope.this.mercury = data.responseData.feed.entries;
-          console.log(data.responseData.feed.entries);
           this.telegraph = News;
           this.bbc = News3;
+
+          $scope.this.mercury = data.responseData.feed.entries;
+          console.log(data.responseData.feed.entries);
+
 
           //return data.responseData.feed.entries;
           // status is the HTTP status
